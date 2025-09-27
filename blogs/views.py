7171d368d7e1d06b9709265f6196d94a06c85d25
gerardoguacarangoitia guacarangoitia <gerardoguacarangoitia@gmail.com>
2025-09-27 17:46:12 -0400

    Agregado el editar post

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
 
 # Create your views here.
 
@@ -13,3 +13,15 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = "post-detail.html"
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "post-new.html"
+    fields = ['title', 'body', 'author']
+    success_url = '/'  # Redirect to home page after successful creation
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = "post-update.html"
+    fields = ['title', 'body']
+    success_url = '/'  # Redirect to home page after successful update

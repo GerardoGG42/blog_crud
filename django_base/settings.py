ed0a016601d477d1d738be9530f925ab0dcf865c
gerardoguacarangoitia guacarangoitia <gerardoguacarangoitia@gmail.com>
2025-10-04 18:08:44 -0400

    Boton cerrar secion  agregado

--- django_base/settings.py
+++ django_base/settings.py
@@ -123,5 +123,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = "post_list"\
+LOGIN_REDIRECT_URL = "post_list"
 LOGOUT_REDIRECT_URL = "post_list"
